8f6f7bd9e2b96b85cd32f82803bbd828811a0819
Team 135 <robotics@phm.k12.in.us>
2019-03-21 08:18:36 -0400

    RobotIsKindamad

--- src/main/java/frc/robot/commands/MotorCommands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/MotorCommands/DriveWithJoysticks.java
@@ -1,34 +1,37 @@
 package frc.robot.commands.MotorCommands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.*;
 
-public class DriveWithJoysticks extends Command 
-{
+public class DriveWithJoysticks extends Command {
 
     private double RightJoystickXValue, RightJoystickYValue, LeftJoystickZValue;
     public boolean isPIDDrive ;
-    public DriveWithJoysticks()
-    {
+    public DriveWithJoysticks() {
         requires(Robot.driveTrain);
     }
 
-    protected void initialize() 
-    {
+    protected void initialize() {
         Robot.limelight.initLimelight(Robot.limelight.LED_OFF, Robot.limelight.DRIVER_CAMERA);
     }
         
-    protected void execute() 
-    {
+    protected void execute() {
         RightJoystickYValue = Robot.oi.GetJoystickYValue(RobotMap.KOI.RIGHT_JOYSTICK) * Robot.oi.returnRightSlider();
         RightJoystickXValue = -Robot.oi.GetJoystickXValue(RobotMap.KOI.RIGHT_JOYSTICK) * Robot.oi.returnRightSlider();
         LeftJoystickZValue = Robot.oi.GetJoystickZValue(RobotMap.KOI.LEFT_JOYSTICK) * Robot.oi.returnLeftSlider();
         Robot.driveTrain.cartesianDrive(RightJoystickXValue, RightJoystickYValue,-LeftJoystickZValue *.40);
+        }
+
+    protected boolean isFinished() {
+        return false;
     }
 
-    protected boolean isFinished() { return false; }
+    protected void end() {
+        Robot.driveTrain.StopMotors();
+    }
 
-    protected void end() {Robot.driveTrain.StopMotors(); }
-
-    protected void interrupted() {this.end(); }
+    protected void interrupted() {
+        this.end();
+    }
 }
